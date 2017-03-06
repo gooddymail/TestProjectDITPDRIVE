7f836936c31b889512265edb0020b9c8bde7f716
Kittitat Saengthong <nest@rgb72.com>
2017-03-06 10:44:16 +0700

    commit

--- TestProjectDITPDRIVE/Router.swift
+++ TestProjectDITPDRIVE/Router.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import Alamofire
+
+enum AlamofireRouter: URLRequestConvertible {
+  case getSpecies
+  
+  var path: String {
+    switch self {
+    case .getSpecies:
+      return "/species"
+    }
+  }
+  
+  var method: Alamofire.HTTPMethod {
+    switch self {
+    case .getSpecies:
+      return .get
+    }
+  }
+  
+  public var parameters: [String: Any]? {
+    switch self {
+    case .getSpecies:
+      return nil
+    }
+  }
+  
+  func asURLRequest() throws -> URLRequest {
+    let baseURLString = "https://swapi.co/api"
+    let url = URL(string: baseURLString + path)!
+    var mutableURLRequest = URLRequest(url: url)
+    mutableURLRequest.httpMethod = method.rawValue
+    
+    return try Alamofire.JSONEncoding.default.encode(mutableURLRequest, with: parameters)
+  }
+}
--- TestProjectDITPDRIVE/ViewController.swift
+++ TestProjectDITPDRIVE/ViewController.swift
@@ -7,19 +7,64 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+  @IBOutlet weak var tableView: UITableView!
+  
+  var species: Array<Any>? = nil
+  var numberOfSpecies = 0
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    getSpecies()
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  func getSpecies() {
+    let router = AlamofireRouter.getSpecies
+    Alamofire.request(router).responseJSON { (response) in
+      switch response.result {
+      case .success(let value):
+        if let jsonResult = value as? [String: Any] {
+//          print("Json Result : \(jsonResult["results"])")
+          
+          self.species = jsonResult["results"] as! Array<Any>!
+          self.numberOfSpecies = (self.species?.count)!
+          
+          self.tableView.reloadData()
+        }
+        
+      case .failure(let error):
+        print(error)
+      }
+    }
+  }
 }
 
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return numberOfSpecies
+}
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    tableView.register(UINib(nibName: "SpeciesTableViewCell", bundle: nil), forCellReuseIdentifier: "SpeciesTableViewCell")
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "SpeciesTableViewCell") as! SpeciesTableViewCell
+    
+    if let specie = species?[indexPath.row] as? [String: Any] {
+      print(specie)
+      
+      if let name = specie["name"] as? String {
+        cell.nameLabel.text = name
+      }
+    }
+    
+    return cell
+  }
+}
