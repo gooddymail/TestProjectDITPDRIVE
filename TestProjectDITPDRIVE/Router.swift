bb0d5d3167c7aa6c3a6c7b7963d2f50d9d3f871b
Kittitat Saengthong <designer@Kittitat-Saengthong-rgb72s-Mac-mini.local>
2017-03-06 10:38:50 +0700

    first commit

---
+++ TestProjectDITPDRIVE/Router.swift
@@ -0,0 +1,9 @@
+//
+//  Router.swift
+//  TestProjectDITPDRIVE
+//
+//  Created by Kittitat Saengthong on 3/3/2560 BE.
+//  Copyright © 2560 Kittitat Saengthong. All rights reserved.
+//
+
+import Foundation
---
+++ TestProjectDITPDRIVE/SpeciesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SpeciesTableViewCell.swift
+//  TestProjectDITPDRIVE
+//
+//  Created by Kittitat Saengthong on 3/3/2560 BE.
+//  Copyright © 2560 Kittitat Saengthong. All rights reserved.
+//
+
+import UIKit
+
+class SpeciesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
